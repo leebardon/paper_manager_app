b99f456150b161708f889ea4abad9073ca199b45
teatauri <leerbardon@gmail.com>
2020-09-15 17:42:06 +0100

    additions to package.json

--- papermanager/frontend/src/components/App.js
+++ papermanager/frontend/src/components/App.js
@@ -4,6 +4,9 @@ import ReactDOM from "react-dom";
 import Header from "./layout/Header";
 import Dashboard from "./papers/Dashboard";
 
+import { Provider } from "react-redux";
+import store from "../store";
+
 class App extends Component {
   render() {
     return (
