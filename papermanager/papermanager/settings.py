b9da202e307b12a9e9941838ff75ccbb9d921d8e
teatauri <leerbardon@gmail.com>
2020-09-15 10:50:13 +0100

    migrations not working...

--- papermanager/papermanager/settings.py
+++ papermanager/papermanager/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'papers',
+    'rest-framework',
 ]
 
 MIDDLEWARE = [
--- papermanager/papers/models.py
+++ papermanager/papers/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Paper(models.Model):
+    title = models.CharField(max_length=200)
+    url = models.URLField(max_length=200, unique=True)
+    main_findings = models.CharField(max_length=1000, blank=True)
+    notes = models.CharField(max_length=1000, blank=True)
+    future_work = models.CharField(max_length=1000, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
